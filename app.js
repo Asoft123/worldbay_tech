38359db405c027579b0020d8b3bd2fac54d72e4f
Festus Alabo Augustine <43374293+Asoft123@users.noreply.github.com>
2022-07-11 20:02:06 +0100

    Added ui fixes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 require("express-async-errors")
 const config = require("config")
+const path = require("path")
 const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const handleFetchResponse = require("./handlers/responseHandle")
 
 const error = require("./middleware/error")
 
 const auth = require("./routes/auth.routes")
-const invalidRoutes = require("./routes/invalid.routes")
+// const invalidRoutes = require("./routes/invalid.routes")
 
 const app = express()
 
@@ -18,6 +18,15 @@ if (!config.get("BASE_URL")) {
 	process.exit(1)
 }
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/client/build")))
+
+	app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running....")
+	})
+}
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -32,18 +41,9 @@ app.use((req, res, next) => {
 	}
 	next()
 })
-// app.get("/", (req, res) => {
-// 	res.status(200).json(
-// 		handleFetchResponse({
-// 			status: false,
-// 			code: 200,
-// 			message: "Welcome to wordbaytech test auth api",
-// 			data: []
-// 		})
-// 	)
-// })
+
 app.use("/api/v1/auth", auth)
-app.use("*", invalidRoutes)
+// app.use("*", invalidRoutes)
 app.use(error)
 
 module.exports = app
